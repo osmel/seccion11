a97046a5b33d949d6c79fc4212dd8e92fa4908cc
Angular CLI <angular-cli@angular.io>
2017-08-27 11:39:40 -0500

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ resumen/¿Qué aprenderemos en esta sección?.php
@@ -0,0 +1,13 @@
+¿Qué aprenderemos en esta sección?
+Sección 11, clase 167
+Esto es lo que veremos a continuación:
+
+1- Uso del modulo HTTP.
+2- Utilizaremos los servicios restful de Firebase
+3- POST.
+4- GET.
+5- DELETE.
+6- PUT.
+7- Configuraciones en la consola de Firebase.
+8- Entre otras cosas útiles.
+Al final de la sección tenemos un examen teórico.
